9d1fae98b28c16ab8fdf084184633110d23681ed
leonardoSilvaMelo <lsilvamelo14@gmail.com>
2024-09-25 18:03:48 -0300

    .

--- src/componentes/ConteudoLateral.js
+++ src/componentes/ConteudoLateral.js
@@ -1,16 +1,24 @@
-import logo from '../imagens/logo-insta.png';
+import logo from '../imagens/tuiter-logo.png';
+import ListItem from '../componentes/ListItem';
+import { RiHome5Fill } from "react-icons/ri";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { IoNotificationsCircle } from "react-icons/io5";
+import { SiImessage } from "react-icons/si";
+import { IoSearchCircle } from "react-icons/io5";
 
 export default function ConteudoLateral() {
     return (
         <div className='conteudoLateral'>
-            <img src={logo} alt='Logo'/>
-            <div>Logo</div>
+            <img src={logo} className='logo' alt='Logo'/>
+            <div>Tuiter Dois</div>
             <div>
-                <ul>
-                    <li>Home</li>
-                    <li>Pesquisar</li>
-                    <li>Explorar</li>
-                    <li>Mensagens</li>
+                <ul className='menuLateral'>
+
+                    <ListItem texto = "Home" icone = {<RiHome5Fill />}/>
+                    <ListItem texto = "Explorar" icone = {<MdOutlineTravelExplore />}/>
+                    <ListItem texto = "Notificações" icone = {<IoNotificationsCircle />}/>
+                    <ListItem texto = "Mensagens" icone = {<SiImessage />}/>
+                    <ListItem texto = "Pesquisar" icone = {<IoSearchCircle />}/>
                 </ul>
             </div>
         </div>
---
+++ src/componentes/ListItem.js
@@ -0,0 +1,8 @@
+export default function ListItem(props){
+    return(
+        <li>
+            <span>{props.icone} </span>
+            <span>{props.texto}</span>
+        </li>
+    )
+};
